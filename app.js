09b7b5ff8cfbee32e6673a4619b1a5ea4dba217f
sathishkumarjs <44187546+sathishkumarjs@users.noreply.github.com>
2018-10-16 16:23:12 +0530

    Update app.js
    Added additional API Calls

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.post('/', function(req, res) {
   });
 });
 
+app.get('/stocks',(req, res)=>{
+   res.send({
+      "quote" : "APPLE",
+      "quote" : "MICROSOFT",
+      "quote" : 'TESLA"
+   }
+});
+
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
